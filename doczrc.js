0673106c45e976b7cb9a5aef23861c495adf4fa3
igorskiter <igor.nw3@gmail.com>
2018-11-15 17:25:47 -0300

    15/11/2018 17h25 - obs:arquivo de configuracao docz

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+    modifyBundlerConfig: (config) => {
+      config.resolve.extensions.push('.scss')
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      })
+  
+      return config
+    }
+  }
